b88a23153105d6c825a84c81dde2786521202ca6
Andrea Lopez <andrea.lopez1904@hotmail.com>
2012-10-02 00:29:02 -0500

    Inicial Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemaApp::Application.config.secret_token = '5cd6c1a489738bb5d4cb4fb2105dedeb34495e646012dc466015576688d4d7fdc7a21744cd4dab8cadce902f13ff7327f43c90bd44c7ab4d79e40a13a1b75fcf'
